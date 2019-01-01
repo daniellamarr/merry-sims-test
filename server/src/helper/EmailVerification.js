6663c716bb90828518afb16ca403cef8d0c31384
Chidinma Ezekwe <chidinmaezekwe@Chidinmas-MacBook-Pro.local>
2019-01-01 10:11:27 +0100

    Removes the sendgrid key

--- server/src/helper/EmailVerification.js
+++ server/src/helper/EmailVerification.js
@@ -1,18 +1,18 @@
 import jwt from 'jsonwebtoken';
-const sendgrid = require('sendgrid')('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');
+import Sendgrid from 'sendgrid';
 import { mail as helper }  from 'sendgrid';
 import models from '../models';
 import dotenv from 'dotenv';
 dotenv.config();
 const { localUsers } = models;
-
+const sendgrid = Sendgrid(process.env.SENDGRID_KEY);
 const secret = process.env.SECRETE_KEY;
 
 class EmailVerification{
   static async sendVerifyLink(req, email, token){
     const host = req.get('host');
     const link = `http://${host}/api/v1/auth/users/verify/${token}`;
-    let from_email = new helper.Email("test@gmail.com");
+    let from_email = new helper.Email("merry@gmail.com");
     let to_email = new helper.Email(email);
     let subject = "Merry verify authors haven link";
     let content = new helper.Content("text/plain", link );
