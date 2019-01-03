9bca963f3b633db6fd64b6acd93dc0e870684891
Proception <omasan.esimaje@gmail.com>
2019-01-03 10:05:17 +0100

    Delete response.js

--- server/src/models/response.js
+++
@@ -1,8 +0,0 @@
-export default class Response {
-  constructor(status, code, messages, data) {
-    this.status = status;
-    this.code = code;
-    this.messages = messages;
-    this.data = data;
-  }
-}
